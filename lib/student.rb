a7cbb0a05d147e1bc6c2cc691ed6fce9f6de2040
Sean Langton <slangton1@gmail.com>
2019-01-05 06:32:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,6 +26,18 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def save
+    if self.id
+      self.update
+    else
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
