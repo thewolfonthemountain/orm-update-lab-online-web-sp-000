0f9c323702ac16327b54856d15713adbfbb9052e
Sean Langton <slangton1@gmail.com>
2019-01-05 06:32:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,6 +38,7 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
