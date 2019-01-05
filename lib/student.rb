b88a9869770a8c4a290a4adcce694a6eef952888
Sean Langton <slangton1@gmail.com>
2019-01-05 06:37:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,11 +47,7 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
+    new_student = self.new(row[1],row[2],row[0])
   end
 
   def self.find_by_name(name)
